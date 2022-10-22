0bf9f4680028e65ced6f16a93df5ce6d9253dbac
Karingomgroblox14 <74888422+karinaomgroblox14@users.noreply.github.com>
2022-10-22 20:02:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,115 @@
+var canvas= document.getElementById("myCanvas");
+
+ctx= canvas.getContext("2d");
+
+var mouseevent ="";
+
+var last_position_of_x, last_position_of_y;
+
+var current_position_of_x, current_position_of_y;
+
+color= "black";
+
+width= 1;
+
+canvaswidth= width- 70
+
+canvasheight= screen.height- 300
+
+
+canvas.addEventListener("mousedown", my_mousedown);
+
+function my_mousedown(e) {
+
+mouseevent="mousedown";
+
+last_position_of_x= e.clientX-canvas.offsetLeft;
+
+last_position_of_y= e.clientY-canvas.offsetTop;
+}
+
+canvas.addEventListener("mouseup", my_mouseup );
+
+function my_mouseup(e) {
+
+mousevent="mouseup";
+}
+
+canvas.addEventListener("mouseleave", my_mouseleave );
+
+function my_mouseleave(e) {
+
+mousevent="mouseleave";
+}
+
+canvas.addEventListener("mousemove", my_mousemove );
+
+function my_mousemove(e) {
+
+current_position_of_x= e.clientX-canvas.offsetLeft;
+
+current_position_of_y= e.clientY-canvas.offsetTop;
+
+if (mouseevent=="mousedown"){
+
+ctx.beginPath();
+
+ctx.strokeStyle= color;
+
+ctx.lineWidth= width;
+
+ctx.moveTo(last_position_of_x, last_position_of_y);
+
+ctx.lineTo(current_position_of_x, current_position_of_y);
+
+ctx.stroke();
+}
+
+
+last_position_of_x= current_position_of_x;
+
+last_position_of_y= current_position_of_y;
+}
+
+if (width<992) {
+
+document.getElementById("myCanvas").width= canvaswidth;
+
+document.getElementById("myCanvas").height= canvasheight;
+
+document.body.style.overflow= "hidden";
+}
+
+addEventListener("touchstart", touchstart);
+
+function touchstart(e) {
+
+last_position_of_x= e.touches[0].clientX- canvas.offsetLeft;
+
+last_position_of_y= e.touches[0].clientY- canvas.offsetTop;
+}
+
+canvas.addEventListener("touchmove", touchmove);
+
+function touchmove(e){
+
+current_position_of_x= e.touches[0].clientX- canvas.offsetLeft;
+
+current_position_of_y= e.touches[0].clientY- canvas.offsetTop;
+
+ctx.beginPath();
+
+ctx.strokeStyle= color;
+
+ctx.linewidth= 3;
+
+ctx.moveTo(last_position_of_x, last_position_of_y);
+
+ctx.lineTo(current_position_of_x, current_position_of_y);
+
+ctx.stroke();
+
+last_position_of_x= current_position_of_x;
+
+last_position_of_y= current_position_of_y;
+}
